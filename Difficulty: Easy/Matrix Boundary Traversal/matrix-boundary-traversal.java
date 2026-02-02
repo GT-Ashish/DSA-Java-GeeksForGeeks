89dfcdb8fc6025f16f470858537d7e9176047c16
Ashish Murarka <murarkaashish6294@gmail.com>
2026-02-02 11:04:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Matrix Boundary Traversal/matrix-boundary-traversal.java
+++ Difficulty: Easy/Matrix Boundary Traversal/matrix-boundary-traversal.java
@@ -2,20 +2,22 @@ class Solution {
     public ArrayList<Integer> boundaryTraversal(int mat[][]) {
         ArrayList<Integer> list = new ArrayList<>();
         int rows = mat.length;
-        int cols = mat.length;
+        int cols = mat[0].length;
         for(int i = 0; i < cols; i++){
            list.add(mat[0][i]);
         }
-        for(int i = 1; i < rows; i++){
-            list.add(mat[i][rows - 1]);
-        }
-        for(int i = cols - 2; i >= 0; i--){
-            list.add(mat[rows - 1][i]);
-        }
-        for(int i = rows - 2; i >= 1; i--){
-            list.add(mat[i][0]);
+        if(rows > 1){
+            for(int i = 1; i < rows; i++){
+                list.add(mat[i][rows - 1]);
+            }
+            for(int i = cols - 2; i >= 0; i--){
+                list.add(mat[rows - 1][i]);
+            }
+            for(int i = rows - 2; i >= 1; i--){
+                list.add(mat[i][0]);
+            }
         }
         return list;
-        
+        //edge case if matrix has only one row or column 
     }
 }
